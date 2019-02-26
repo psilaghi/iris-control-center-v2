a67ea888b8295014b303f160aba3f2de7198686f
Paul Silaghi <psilaghi@mozilla.com>
2019-02-26 16:57:31 +0200

    runs page table updates

--- src/components/AllRunsPage/index.js
+++ src/components/AllRunsPage/index.js
@@ -13,34 +13,50 @@ const StyledReactTable = styled(ReactTable)`
   padding: 50px;
 `;
 
+const Logo = styled.img`
+  height: 27px;
+`;
+
 const TABLE_COLUMNS = [{
-  Header: "ID",
-  accessor: "id",
-  Cell: row => (<Link to={`/runs/${row.value}`}>{row.value}</Link>),
-  className: "table__cell table__cell--centered"
-}, {
-  Header: "Build",
-  accessor: "build",
-  className: "table__cell table__cell--centered"
-}, {
-  Header: "Channel",
-  accessor: "channel",
-  className: "table__cell table__cell--centered"
-}, {
-  Header: "Failed",
   accessor: "failed",
+  className: "table__cell table__cell--centered"
+}, {
+  Header: "App",
+  accessor: "target",
+  Cell: data => <Logo src={`/images/${data.value}.png`} />,
+  className: "table__cell table__cell--centered"
+}, {
+  Header: "ID #",
+  accessor: "id",
   className: "table__cell table__cell--centered"
 }, {
   Header: "Locale",
   accessor: "locale",
   className: "table__cell table__cell--centered"
 }, {
-  Header: "Total",
+  Header: "Total Tests",
   accessor: "total",
   className: "table__cell table__cell--centered"
 }, {
-  Header: "Version",
-  accessor: "version",
+  Header: "Duration",
+  accessor: "duration",
+  Cell: seconds => {
+    seconds = Number(seconds.value);
+    var d = Math.floor(seconds / (3600*24));
+    var h = Math.floor(seconds % (3600*24) / 3600);
+    var m = Math.floor(seconds % 3600 / 60);
+    var s = Math.floor(seconds % 3600 % 60);
+
+    var dDisplay = d > 0 ? d + (d == 1 ? " day " : " days ") : "";
+    var hDisplay = h > 0 ? h + (h == 1 ? " hr " : " hrs ") : "";
+    var mDisplay = m > 0 ? m + (m == 1 ? " min " : " min ") : "";
+    var sDisplay = s > 0 ? s + (s == 1 ? " sec" : " sec") : "";
+    return dDisplay + hDisplay + mDisplay + sDisplay;
+  },
+  className: "table__cell table__cell--centered"
+}, {
+  Header: "Completed",
+  accessor: "id",
   className: "table__cell table__cell--centered"
 }];
 
@@ -51,40 +67,40 @@ class AllRunsPage extends React.Component {
   }
 
   componentDidMount() {
-    ApiClient.get('/data/all_runs.json').then(response => this.setState({runs: response.runs}));
+    ApiClient.get('/data/runs.json').then(response => this.setState({runs: response.runs}));
   }
 
-  handleDelete = (id) => {
-    ApiClient.get(`/delete?${id}`);
-    this.setState({runs: this.state.runs.filter(run => run.id !== id)});
+  // handleDelete = (id) => {
+  //   ApiClient.get(`/delete?${id}`);
+  //   this.setState({runs: this.state.runs.filter(run => run.id !== id)});
 
-  }
+  // }
 
-  _getColumns = () => [...TABLE_COLUMNS, {
-      Header: "Actions",
-      id: "action",
-      accessor: data => data.id,
-      Cell: row => (
-        <div>
-          <Link className="btn btn-primary table__cell-btn" role="button" to={`/runs/${row.value}`}>
-            <FontAwesomeIcon icon={faEye} size="lg" />
-          </Link>
+  // _getColumns = () => [...TABLE_COLUMNS, {
+  //     Header: "Actions",
+  //     id: "action",
+  //     accessor: data => data.id,
+  //     Cell: row => (
+  //       <div>
+  //         <Link className="btn btn-primary table__cell-btn" role="button" to={`/runs/${row.value}`}>
+  //           <FontAwesomeIcon icon={faEye} size="lg" />
+  //         </Link>
 
-          <button className="btn btn-danger table__cell-btn" onClick={() => this.handleDelete(row.value)}>
-            <FontAwesomeIcon icon={faTrash} size="lg" />
-          </button>
-        </div>
-      ),
-      className: "table__cell table__cell--centered"
-    }
-  ]
+  //         <button className="btn btn-danger table__cell-btn" onClick={() => this.handleDelete(row.value)}>
+  //           <FontAwesomeIcon icon={faTrash} size="lg" />
+  //         </button>
+  //       </div>
+  //     ),
+  //     className: "table__cell table__cell--centered"
+  //   }
+  // ]
 
   render() {
     return (
       <div className="page">
         <StyledReactTable
           data={this.state.runs}
-          columns={this._getColumns()}
+          columns={TABLE_COLUMNS}
           className="-striped -highlight"
           defaultPageSize={5}
           defaultSorted={[
