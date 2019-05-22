e093901f1703e5e4593ee495b28e47f07f1575b5
Paul Silaghi <psilaghi@mozilla.com>
2019-05-22 12:45:58 +0300

    FailedTestDetails updates

--- src/components/AllRunsPage/RunDetails/FailedTestDetails.js
+++ src/components/AllRunsPage/RunDetails/FailedTestDetails.js
@@ -12,8 +12,6 @@ const Container = styled.div`
 const Details = styled.div`
   word-wrap: break-word;
   font-size: 12px;
-  /* padding-left: 10px; */
-  background-color: #f9f9fa;
   margin: 10px;
 `;
 const Title = styled.div`
@@ -24,8 +22,16 @@ const Title = styled.div`
 const Detail = styled.div`
   padding-bottom: 12px;
 `;
-const DetailTitle = styled.i`
+const DetailKey = styled.i`
   color: #0060df;
+`;
+const DetailValue = styled.div``;
+const AssertKey = styled.div`
+  color: #0060df;
+  font-weight: bold;
+`;
+const AssertValue = styled.div`
+  background-color: #f9f9fa;
 `;
 const Description = styled.div`
   font-size: 15px;
@@ -41,7 +47,7 @@ const TitleSummary = styled.div`
   background-color: #f9f9fa;
   overflow: auto;
 `;
-const AssertContainer = styled.div`
+const DataContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
@@ -52,6 +58,7 @@ const AssertContainer = styled.div`
   padding: 0 30px 0 10px;
   margin: 10px 0;
   font-size: 18px;
+  ${props => props.expanded && 'border-bottom: 1px solid #D7D7DB;'}
 `;
 const AssertSummary = styled.div`
   display: flex;
@@ -87,39 +94,74 @@ const ThumbnailButton = styled.button`
   cursor: pointer;
 `;
 
+const withExpand = WrappedComponent => {
+  class WithExpand extends React.Component {
+    state = {
+      expanded: false
+    };
+
+    handleToggleClick = () => {
+      this.setState({
+        expanded: !this.state.expanded
+      });
+    };
+
+    render() {
+      return (
+        <WrappedComponent
+          {...this.props}
+          expanded={this.state.expanded}
+          onToggleClick={this.handleToggleClick}
+        />
+      );
+    }
+  }
+
+  return WithExpand;
+};
+
 class FailedTestDetails extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      expanded: false
+      assertsSection: false,
+      detailsSection: false
     };
   }
 
-  toggleCollapse = event => {
-    if (event.target.type !== 'checkbox') {
-      this.setState({
-        expanded: !this.state.expanded
-      });
-    }
+  toggleCollapse = expandedSection => {
+    this.setState({
+      [expandedSection]: !this.state[expandedSection]
+    });
   };
 
-  renderDetails = (data, keyName) => (
-    <Details key={keyName}>
-      {Object.keys(data).map(key =>
-        data[key] && typeof data[key] === 'object' ? (
-          <Detail key={key}>
-            <DetailTitle>{key}: </DetailTitle>
-            {this.renderDetails(data[key], key)}
-          </Detail>
-        ) : (
-          <Detail key={key}>
-            <DetailTitle>{key}: </DetailTitle>
-            {data[key] + ''}
-          </Detail>
-        )
-      )}
-    </Details>
-  );
+  renderDetails = (data, keyName, KeyComponent, ValueComponent) =>
+    withExpand(function(props) {
+      return (
+        <Details key={keyName}>
+          {Object.keys(data).map(key =>
+            data[key] && typeof data[key] === 'object' ? (
+              <Detail key={key}>
+                <ExpandButton type="button" onClick={props.onToggleClick}>
+                  <ExpandIcon expanded={props.expanded} icon="CarrotRight" />
+                </ExpandButton>
+                <KeyComponent>{key}: </KeyComponent>
+                {props.expanded && (
+                  <ValueComponent>
+                    {this.renderDetails(data[key], key, KeyComponent, ValueComponent)}
+                  </ValueComponent>
+                )}
+              </Detail>
+            ) : (
+              <Detail key={key}>
+                <KeyComponent>{key}: </KeyComponent>
+                <ValueComponent>{data[key] + ''}</ValueComponent>
+              </Detail>
+            )
+          )}
+        </Details>
+      );
+    });
 
   render() {
     const { assert, debug_images, ...details } = this.props.test;
@@ -130,32 +172,33 @@ class FailedTestDetails extends React.Component {
           <Description>{this.props.test.description}</Description>
         </TitleSummary>
 
-        <AssertContainer>
+        <DataContainer expanded={this.state.assertsSection}>
           <AssertSummary>
             <StyledWarningIcon icon="Warning" />
             <span>Failed Assert:&nbsp;</span>
             <span>{this.props.test.assert.message}</span>
           </AssertSummary>
-          <ExpandButton type="button" onClick={this.toggleCollapse}>
-            <ExpandIcon expanded={this.state.expanded} icon="arrowhead-right" />
+          <ExpandButton type="button" onClick={() => this.toggleCollapse('assertsSection')}>
+            <ExpandIcon expanded={this.state.assertsSection} icon="arrowhead-right" />
           </ExpandButton>
-        </AssertContainer>
-        {this.state.expanded && this.renderDetails(assert, 'assert')}
+        </DataContainer>
+        {this.state.assertsSection && this.renderDetails(assert, 'assert', AssertKey, AssertValue)}
 
-        <AssertContainer>
+        <DataContainer>
           <span>Debug images</span>
           <ThumbnailButton type="button">
             <Icon icon="ThumbnailIcon" />
           </ThumbnailButton>
-        </AssertContainer>
+        </DataContainer>
 
-        <AssertContainer>
+        <DataContainer expanded={this.state.detailsSection}>
           <span>Details</span>
-          <ExpandButton type="button" onClick={this.toggleCollapse}>
-            <ExpandIcon expanded={this.state.expanded} icon="arrowhead-right" />
+          <ExpandButton type="button" onClick={() => this.toggleCollapse('detailsSection')}>
+            <ExpandIcon expanded={this.state.detailsSection} icon="arrowhead-right" />
           </ExpandButton>
-        </AssertContainer>
-        {this.state.expanded && this.renderDetails(details, 'assert')}
+        </DataContainer>
+        {this.state.detailsSection &&
+          this.renderDetails(details, 'details', DetailKey, DetailValue)}
       </Container>
     );
   }
